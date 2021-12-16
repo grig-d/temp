9579e465d0fddf9dcce152f2e425e5cac36fe0b9
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-12-16 20:28:26 +0200

    reset fn done

--- main.js
+++ main.js
@@ -1,24 +1,84 @@
 const ref = {
-  container: document.querySelector('.container'),
-  row: document.querySelector('.row-list'),
+  // container: document.querySelector('.container'),
+  row: document.getElementById('row'),
+  resBtn: document.getElementById('res'),
+  delBtn: document.getElementById('del'),
+  addBtn: document.getElementById('add'),
 };
 
-ref.container.addEventListener('click', message);
-
-function message(event) {
-  if(event.target.tagName === 'LI') {
-  console.log(event.target.tagName);
-  console.log(event.target.parentElement.tagName);
-  event.target.parentElement.append('li');
+function reset() {
+  ref.delBtn.disabled = false;
+  ref.row.textContent = '';
+  for (let i = 0; i < 8; i++) {
+    addColumn();
   }
-
 }
 
+reset();
+ref.resBtn.addEventListener('click', reset);
 
-console.log(ref);
-console.log(ref.container);
-console.log(ref.row);
-// function makeBlink () {
-//     // body
+
+// ref.container.addEventListener('click', message);
+
+// function message(event) {
+//   if(event.target.tagName === 'LI') {
+//   console.log(event.target.tagName);
+//   console.log(event.target.parentElement.tagName);
+//   event.target.parentElement.append('li');
+//   }
+
 // }
 
+// console.log(ref);
+// console.log(ref.container);
+// console.log(ref.row);
+// // function makeBlink () {
+// //     // body
+// // }
+
+// function removeColumn() {
+//   console.log('do');
+//   const row = document.getElementById('row');
+//   row.removeChild(row.lastChild);
+// }
+
+// ref.del.addEventListener('click', removeColumn);
+
+//
+function addColumn() {
+  const select = document.getElementById('SELECT_country');
+  const clone = select.cloneNode(true);
+
+  clone.textContent = 'SELECT_country';
+
+  document.getElementById('row').appendChild(clone);
+
+  if (document.getElementById('row').childNodes.length > 0) {
+    ref.delBtn.disabled = false;
+  }
+
+  createColumn();
+}
+
+function deleteColumn() {
+  const select = document.getElementById('row');
+  if (select.childNodes.length > 0) {
+    select.removeChild(select.lastChild);
+  }
+  if (select.childNodes.length === 0) {
+    ref.delBtn.disabled = true;
+  }
+}
+
+document.getElementById('add').addEventListener('click', addColumn);
+ref.delBtn.addEventListener('click', deleteColumn);
+
+function createColumn() {
+  const column = document.createElement('div');
+  console.log(column);
+}
+
+// function createQuad() {
+//   const quad = document.createElement('div');
+//   console.log(quad);
+// }
