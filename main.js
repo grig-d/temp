c26da08663f3ea29ccc34069e740cad122134bec
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-12-16 21:09:55 +0200

    buttons functions done

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const ref = {
-  // container: document.querySelector('.container'),
   row: document.getElementById('row'),
   resBtn: document.getElementById('res'),
   delBtn: document.getElementById('del'),
@@ -15,10 +14,40 @@ function reset() {
 }
 
 reset();
+
+function createColumn() {
+  const newColumn = document.createElement('div');
+  newColumn.classList.add('column');
+  newColumn.appendChild(createQuad());
+  return newColumn;
+}
+
+function addColumn() {
+  ref.row.appendChild(createColumn());
+  if (ref.row.childNodes.length) {
+    ref.delBtn.disabled = false;
+  }
+}
+
+function deleteColumn() {
+  const select = document.getElementById('row');
+  if (select.childNodes.length > 0) {
+    select.removeChild(select.lastChild);
+  }
+  if (select.childNodes.length === 0) {
+    ref.delBtn.disabled = true;
+  }
+}
+
+function createQuad() {
+  const newQuad = document.createElement('div');
+  newQuad.classList.add('quad');
+  return newQuad;
+}
+
 ref.resBtn.addEventListener('click', reset);
-
-
-// ref.container.addEventListener('click', message);
+ref.delBtn.addEventListener('click', deleteColumn);
+ref.addBtn.addEventListener('click', addColumn);
 
 // function message(event) {
 //   if(event.target.tagName === 'LI') {
@@ -45,40 +74,5 @@ ref.resBtn.addEventListener('click', reset);
 // ref.del.addEventListener('click', removeColumn);
 
 //
-function addColumn() {
-  const select = document.getElementById('SELECT_country');
-  const clone = select.cloneNode(true);
 
-  clone.textContent = 'SELECT_country';
 
-  document.getElementById('row').appendChild(clone);
-
-  if (document.getElementById('row').childNodes.length > 0) {
-    ref.delBtn.disabled = false;
-  }
-
-  createColumn();
-}
-
-function deleteColumn() {
-  const select = document.getElementById('row');
-  if (select.childNodes.length > 0) {
-    select.removeChild(select.lastChild);
-  }
-  if (select.childNodes.length === 0) {
-    ref.delBtn.disabled = true;
-  }
-}
-
-document.getElementById('add').addEventListener('click', addColumn);
-ref.delBtn.addEventListener('click', deleteColumn);
-
-function createColumn() {
-  const column = document.createElement('div');
-  console.log(column);
-}
-
-// function createQuad() {
-//   const quad = document.createElement('div');
-//   console.log(quad);
-// }
