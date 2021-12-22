9e91290d32885016db855d9a3fa8c6efcf677134
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-12-22 19:36:16 +0200

    client local storage start

---
+++ main.js
@@ -0,0 +1,56 @@
+const book = {
+  name: 'Охота на фазана',
+  author: 'Марта Кэтра',
+  genre: 'детектив',
+  pageCount: 724,
+  publisher: 'ООО Астрель',
+};
+
+console.log(book);
+console.log(typeof book);
+
+const bookStringified = JSON.stringify(book);
+console.log(bookStringified);
+console.log(typeof bookStringified);
+
+const bookParsed = JSON.parse(bookStringified);
+console.log(bookParsed);
+console.log(typeof bookParsed);
+
+////
+
+localStorage.setItem('key', 'value');
+const value = localStorage.getItem('key');
+console.log('value from localStorage: ', value);
+
+const settings = {
+  theme: 'dark',
+  fontSize: 14,
+};
+
+localStorage.setItem('settings', JSON.stringify(settings));
+
+localStorage.removeItem('key');
+
+
+////////////VENIK
+
+const clientStorage = {
+  getItem(key) {
+    try {
+      JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      return undefined;
+    }
+  },
+  setItem(key, payload) {
+    try {
+      localStorage.setItem(key, JSON.stringify(payload));
+    } catch (error) {
+      return undefined;
+    }
+  },
+};
+
+
+//////////////////////
